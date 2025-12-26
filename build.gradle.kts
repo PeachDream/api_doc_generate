cb9d3e1dcfe74aa48e241314733abfa158659b15
税先桃 <275092531@qq.com>
2025-12-26 17:03:00 +0800

    feat(api-doc): 在JSON示例中添加字段类型注释
    - 用带注释的JSON值替换默认值显示，格式为“类型 //描述”
    - 在生成JSON时给基本类型字段添加类型及描述注释
    - 优化设置对话框尺寸，采用屏幕相对大小并限制最大最小值
    - 调整预览对话框表头样式，使用透明背景及自定义颜色和边框
    - 注册通知组以支持复制和导出成功提示的非模态通知

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,13 @@ repositories {
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
     intellijPlatform {
-        intellijIdea("2025.2.4")
+        // 方式1: 使用本地已安装的 IDEA（避免下载）
+        // 请将路径修改为你的 IDEA 安装目录
+        local("C:/Program Files/JetBrains/IntelliJ IDEA 2024.3.1")
+        
+        // 方式2: 从网络下载指定版本（注释掉上面的 local，取消下面的注释）
+        // intellijIdea("2024.3")
+        
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
 
@@ -29,25 +35,32 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "252.25557"
+            // 支持 IDEA 2024.1 及以上版本
+            // 241 = IDEA 2024.1, 242 = IDEA 2024.2, 243 = IDEA 2024.3
+            // 251 = IDEA 2025.1, 252 = IDEA 2025.2
+            sinceBuild = "241"
+            // 不设置 untilBuild 表示支持所有未来版本
+            // 如需限制最高版本，可设置如: untilBuild = "243.*"
         }
 
         changeNotes = """
-            Initial version
+            Initial version - 支持 IDEA 2024.1+
         """.trimIndent()
     }
 }
 
 tasks {
     // Set the JVM compatibility versions
+    // IDEA 2024.x 使用 Java 17，IDEA 2025.x 使用 Java 21
+    // 为了兼容 2024，使用 Java 17
     withType<JavaCompile> {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
